4f30bd6f92ab3bdb8861b9836bdbbca3fe766dc1
andrew astakhov <astakho1@msu.edu>
2015-10-06 20:17:42 -0400

    multiplication feature added

--- Calculator_1.py
+++ Calculator_1.py
@@ -2,11 +2,16 @@ __author__ = 'jasonmerlo'
 
 
 def main():
+# Add
     num_a = int(input("Enter a number to add "))
     num_b = int(input("Enter another number to add: "))
     print("{}".format(num_a + num_b))
+# Subtract
 num a = int(input("Enter a number to subtract from: "))
 num b = int(input("Enter a second number to subtract: "))
 print("{}".format(num_a - num_b))
-
+# Multiple
+num a = int(input("Enter a number to multiply: "))
+num b = int(input("Enter a second number to multiply: "))
+print("{}".format(num_a * num_b))
 main()
